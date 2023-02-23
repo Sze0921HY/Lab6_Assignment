4b071c7d9e116f89cce91caef114b65302edc516
SzeHY <125120294+SzeHY@users.noreply.github.com>
2023-02-23 18:41:55 -0500

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,9 +2,10 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	int m = (low + high)/2;
 	if (low > hight)
 		return -1;
-	int m = (low + high)/2;
+	if (value > numbers[m])
 		return search(numbers, mid + 1, high, value);
 	else if (value < numbers[m])
 		return search(numbers, low, m-1, value);
